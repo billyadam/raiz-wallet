f35dd6206962bfec0fa6c3ec7f3015bf97f8df7b
Billy <bill@wartek.belajar.id>
2024-09-17 06:20:13 +0700

    fix indentation in user controller test

--- spec/controllers/api/users_controller_test.rb
+++ spec/controllers/api/users_controller_test.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Api::UsersController, type: :controller do
 	let(:auth_token) { 'xyz123zbc456tuxyz123zbc456tu' }
 
-  let (:username) { "johny" }
-  let (:password) { "password" }
-  let (:params) { { username: username, password: password } }
+	let (:username) { "johny" }
+	let (:password) { "password" }
+	let (:params) { { username: username, password: password } }
 
-  let (:login_service) { double(UserService::LoginService) }
+	let (:login_service) { double(UserService::LoginService) }
 
 	describe '#login' do
 		context 'when login success' do
@@ -19,8 +19,8 @@ RSpec.describe Api::UsersController, type: :controller do
 			}}
 			
 			it 'return 200' do
-        allow(UserService::LoginService).to receive(:new).with(username, password).and_return(login_service)
-        allow(login_service).to receive(:login).and_return(auth_token)
+				allow(UserService::LoginService).to receive(:new).with(username, password).and_return(login_service)
+				allow(login_service).to receive(:login).and_return(auth_token)
 
 				post :login, params: params
 
@@ -32,7 +32,7 @@ RSpec.describe Api::UsersController, type: :controller do
 		context 'when user not found' do
 			it 'return 404' do
 				allow(UserService::LoginService).to receive(:new).with(username, password).and_return(login_service)
-        allow(login_service).to receive(:login).and_raise(ActiveRecord::RecordNotFound, I18n.t('errors.user_not_found'))
+				allow(login_service).to receive(:login).and_raise(ActiveRecord::RecordNotFound, I18n.t('errors.user_not_found'))
 
 				post :login, params: params
 
@@ -45,7 +45,7 @@ RSpec.describe Api::UsersController, type: :controller do
 		context 'when invalid password' do
 			it 'return 401' do
 				allow(UserService::LoginService).to receive(:new).with(username, password).and_return(login_service)
-        allow(login_service).to receive(:login).and_raise(UnauthorizedError, I18n.t('errors.invalid_password'))
+				allow(login_service).to receive(:login).and_raise(UnauthorizedError, I18n.t('errors.invalid_password'))
 
 				post :login, params: params
 
@@ -55,11 +55,11 @@ RSpec.describe Api::UsersController, type: :controller do
 			end
 		end
 
-    context 'when service returns other error' do
-      let(:other_error) { "other error" }
-      it 'return 500' do
+		context 'when service returns other error' do
+			let(:other_error) { "other error" }
+			it 'return 500' do
 				allow(UserService::LoginService).to receive(:new).with(username, password).and_return(login_service)
-        allow(login_service).to receive(:login).and_raise(StandardError, other_error)
+				allow(login_service).to receive(:login).and_raise(StandardError, other_error)
 
 				post :login, params: params
 
