9999b6df54e9bbc80531ffc3497c919e3ba59ec0
Billy <bill@wartek.belajar.id>
2024-09-16 18:39:51 +0700

    unit test for model login

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#login" do
+    let(:token) { "sfdusjjosdfkl" }
+    let(:user) { User.new(username: "jojo", name: "Jojo", password: BCrypt::Password.create("password")) }
+
+    it "returns the token correctly" do
+      expect(Session).to receive(:new).and_call_original
+      expect(SecureRandom).to receive(:hex).with(20).and_return(token)
+      allow_any_instance_of(Session).to receive(:save!).and_return(true)
+
+      token_res = Session.login(user)
+      expect(token_res).to eq(token)
+    end
+  end
 end
