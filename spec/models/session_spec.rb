a0eb9f0b3f768ed628652e4f2cae816553864014
Billy <bill@wartek.belajar.id>
2024-09-17 05:29:08 +0700

    fix tabbing

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
-  let(:token) { "xyzabc12345678" }
-  let(:user) { User.new(username: "jojo", name: "Jojo", password: BCrypt::Password.create("password")) }
-  
-  describe ".login" do
-    it "returns the token correctly" do
-      expect(Session).to receive(:new).and_call_original
-      expect(SecureRandom).to receive(:hex).with(20).and_return(token)
-      allow_any_instance_of(Session).to receive(:save!).and_return(true)
+	let(:token) { "xyzabc12345678" }
+	let(:user) { User.new(username: "jojo", name: "Jojo", password: BCrypt::Password.create("password")) }
+	
+	describe ".login" do
+		it "returns the token correctly" do
+			expect(Session).to receive(:new).and_call_original
+			expect(SecureRandom).to receive(:hex).with(20).and_return(token)
+			allow_any_instance_of(Session).to receive(:save!).and_return(true)
 
-      token_res = Session.login(user)
-      expect(token_res).to eq(token)
-    end
-  end
+			token_res = Session.login(user)
+			expect(token_res).to eq(token)
+		end
+	end
 
-  describe ".get_active_session" do
-    let(:session) { Session.new(token: token, user: user, expired_at: 1.day.from_now) }
+	describe ".get_active_session" do
+		let(:session) { Session.new(token: token, user: user, expired_at: 1.day.from_now) }
 
-    it "returns the session correctly" do
-      expect(Session).to receive(:where).with('token = ? AND expired_at > ?', token, anything).and_return([session])
+		it "returns the session correctly" do
+			expect(Session).to receive(:where).with('token = ? AND expired_at > ?', token, anything).and_return([session])
 
-      session_res = Session.get_active_session(token)
-      expect(session_res).to eq(session)
-    end
-  end
+			session_res = Session.get_active_session(token)
+			expect(session_res).to eq(session)
+		end
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:password) { BCrypt::Password.create("password123")}
-  let(:user) { User.new(name: "Johnny", username: "johnny", password: password) }
+	let(:password) { BCrypt::Password.create("password123")}
+	let(:user) { User.new(name: "Johnny", username: "johnny", password: password) }
 
-  describe '#authenticate' do
-    context 'when password is correct' do
-      it 'return true' do
-        expect(user.authenticate("password123")).to eq(true)
-      end
-    end
+	describe '#authenticate' do
+		context 'when password is correct' do
+			it 'return true' do
+				expect(user.authenticate("password123")).to eq(true)
+			end
+		end
 
-    context 'when password is incorrect' do
-      it 'return false' do
-        expect(user.authenticate("password")).to eq(false)
-      end
-    end
-  end
+		context 'when password is incorrect' do
+			it 'return false' do
+				expect(user.authenticate("password")).to eq(false)
+			end
+		end
+	end
 end
