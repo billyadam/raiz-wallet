744db6d5afab3e622a666277659b8fb6fb85b06e
Billy <bill@wartek.belajar.id>
2024-09-17 05:27:51 +0700

    complete unit test for session and fix # for .

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
-  describe "#login" do
-    let(:token) { "sfdusjjosdfkl" }
-    let(:user) { User.new(username: "jojo", name: "Jojo", password: BCrypt::Password.create("password")) }
-
+  let(:token) { "xyzabc12345678" }
+  let(:user) { User.new(username: "jojo", name: "Jojo", password: BCrypt::Password.create("password")) }
+  
+  describe ".login" do
     it "returns the token correctly" do
       expect(Session).to receive(:new).and_call_original
       expect(SecureRandom).to receive(:hex).with(20).and_return(token)
@@ -12,6 +12,17 @@ RSpec.describe Session, type: :model do
 
       token_res = Session.login(user)
       expect(token_res).to eq(token)
+    end
+  end
+
+  describe ".get_active_session" do
+    let(:session) { Session.new(token: token, user: user, expired_at: 1.day.from_now) }
+
+    it "returns the session correctly" do
+      expect(Session).to receive(:where).with('token = ? AND expired_at > ?', token, anything).and_return([session])
+
+      session_res = Session.get_active_session(token)
+      expect(session_res).to eq(session)
     end
   end
 end
--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Transfer, type: :model do
-    describe "#transfer" do
+    describe ".transfer" do
         let(:src_wallet) { Wallet.new(id: 1, address: "xyz12345") }
         let(:dest_wallet) { Wallet.new(id: 2, address: "abc56789") }
         let(:amount) { 50 }
