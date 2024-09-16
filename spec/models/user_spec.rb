d1b321852d0e67db38b75a56bef3efafda9d0290
Billy <bill@wartek.belajar.id>
2024-09-16 18:26:13 +0700

    added unit test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:password) { BCrypt::Password.create("password123")}
+  let(:user) { User.new(name: "Johnny", username: "johnny", password: password) }
+
+  describe '#authenticate' do
+    context 'when password is correct' do
+      it 'return true' do
+        expect(user.authenticate("password123")).to eq(true)
+      end
+    end
+
+    context 'when password is incorrect' do
+      it 'return false' do
+        expect(user.authenticate("password")).to eq(false)
+      end
+    end
+  end
 end
